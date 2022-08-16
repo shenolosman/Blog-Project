f78814b77610366e6a6c9baeb48a064e7c0df0d8
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-08-16 19:50:50 +0200

    Appuser login dto

---
+++ BlogProject.DTO/DTOs/AppUser/AppUserLoginDto.cs
@@ -0,0 +1,10 @@
+﻿using BlogProject.DTO.Interface;
+
+namespace BlogProject.DTO.DTOs.AppUser
+{
+    public class AppUserLoginDto : IDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
