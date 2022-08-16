204509d963ac18e23afd94cced80cfa39091c7e8
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-08-16 19:03:18 +0200

    Web API created

---
+++ BlogProject.WebApi/Controllers/BlogsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+    }
+}
