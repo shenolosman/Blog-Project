b08751bb440111e8ec29e90445509e74bda0f7bd
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-08-16 19:56:39 +0200

    AutoMapper nuget package added to api

---
+++ BlogProject.WebApi/Mapping/AutoMapperProfile/MapProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BlogProject.DTO.DTOs.Blog;
+using BlogProject.DTO.DTOs.Category;
+using BlogProject.Entities.Concrete;
+using BlogProject.WebApi.Models;
+
+namespace BlogProject.WebApi.Mapping.AutoMapperProfile
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<BlogListDto, Blog>();
+            CreateMap<Blog, BlogListDto>();
+
+            CreateMap<BlogUpdateModel, Blog>();
+            CreateMap<Blog, BlogUpdateModel>();
+
+            CreateMap<BlogAddModel, Blog>();
+            CreateMap<Blog, BlogAddModel>();
+
+            CreateMap<CategoryListDto, Category>();
+            CreateMap<Category, CategoryListDto>();
+
+            CreateMap<CategoryAddDto, Category>();
+            CreateMap<Category, CategoryAddDto>();
+
+            CreateMap<CategoryUpdateDto, Category>();
+            CreateMap<Category, CategoryUpdateDto>();
+        }
+    }
+}
