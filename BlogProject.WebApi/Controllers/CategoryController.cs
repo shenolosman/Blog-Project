f7f4e3674dd15299fb1fc27f6560e1139afd1703
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-08-18 22:24:29 +0200

    created category contoroller api

---
+++ BlogProject.WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using BlogProject.Business.Interfaces;
+using BlogProject.DTO.DTOs.Category;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(IMapper mapper, ICategoryService categoryService)
+        {
+            _mapper = mapper;
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_mapper.Map<List<CategoryListDto>>(await _categoryService.GetAllSortedByIdAsync()));
+        }
+    }
+}
