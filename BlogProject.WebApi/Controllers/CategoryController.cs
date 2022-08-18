ea9ee9e33321355d093fd65d21fa010d4f5080d1
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-08-18 22:43:41 +0200

    category controller crud is done

--- BlogProject.WebApi/Controllers/CategoryController.cs
+++ BlogProject.WebApi/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BlogProject.Business.Interfaces;
 using BlogProject.DTO.DTOs.Category;
+using BlogProject.Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogProject.WebApi.Controllers
@@ -17,11 +18,38 @@ namespace BlogProject.WebApi.Controllers
             _mapper = mapper;
             _categoryService = categoryService;
         }
-
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             return Ok(_mapper.Map<List<CategoryListDto>>(await _categoryService.GetAllSortedByIdAsync()));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(_mapper.Map<CategoryListDto>(await _categoryService.FindByIdAsync(id)));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryAddDto model)
+        {
+            await _categoryService.AddAsync(_mapper.Map<Category>(model));
+            return Created("", model);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, CategoryUpdateDto model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest("Id does not match");
+            }
+            await _categoryService.UpdateAsync(_mapper.Map<Category>(model));
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryService.RemoveAsync(new Category() { Id = id });
+            return NoContent();
         }
     }
 }
