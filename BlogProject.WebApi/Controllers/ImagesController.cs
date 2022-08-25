728d18dd5c75b5517f0147bef254cd9e02927969
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-08-25 22:53:24 +0200

    imagecontroller to get images from api

---
+++ BlogProject.WebApi/Controllers/ImagesController.cs
@@ -0,0 +1,28 @@
+﻿using BlogProject.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IBlogService blogService;
+
+        public ImagesController(IBlogService blogService)
+        {
+            this.blogService = blogService;
+        }
+
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> GetBlogImageById(int id)
+        {
+            var blog = await blogService.FindByIdAsync(id);
+            if (string.IsNullOrWhiteSpace(blog.ImagePath))
+            {
+                return NotFound("No Image");
+            }
+            return File($"/img/{blog.ImagePath}", "image/jpeg");
+        }
+    }
+}
