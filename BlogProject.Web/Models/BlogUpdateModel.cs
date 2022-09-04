928bbeaab8a3d5c3be9ac77bb3f94a36bc930bb2
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-09-04 14:53:09 +0200

    adding trumbowyg addon for deatil editable blog porsts

---
+++ BlogProject.Web/Models/BlogUpdateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogProject.Web.Models
+{
+    public class BlogUpdateModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public int AppUserId { get; set; }
+    }
+}
